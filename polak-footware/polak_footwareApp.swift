f62e7e9724a2815dd2b5633c16851def42d8bacb
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-10-12 21:39:20 +0300

    Initial Commit

---
+++ polak-footware/polak_footwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  polak_footwareApp.swift
+//  polak-footware
+//
+//  Created by MAC on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct polak_footwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
