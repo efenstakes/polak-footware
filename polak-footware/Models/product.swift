be35da7de396f3f813039ccf5a9c7cb631ead314
kipchumba26 <cynthia.kipchumba@nativecoders.se>
2021-10-13 16:23:09 +0300

    add product model & sample data

--- polak-footware/Models/product.swift
+++ polak-footware/Models/product.swift
@@ -6,3 +6,79 @@
 //
 
 import Foundation
+
+
+struct Product: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var price: Float
+    var image: String
+    var isFavorite: Bool = false
+}
+
+var products: [Product] = [
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2000,
+        image: "1",
+        isFavorite: true
+    ),
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2000,
+        image: "2",
+        isFavorite: true
+    ),
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2000,
+        image: "3",
+        isFavorite: true
+    ),
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2000,
+        image: "4",
+        isFavorite: true
+    ),
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2000,
+        image: "5",
+        isFavorite: true
+    ),
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2000,
+        image: "6",
+        isFavorite: true
+    ),
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2700,
+        image: "7",
+        isFavorite: true
+    ),
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2000,
+        image: "8",
+        isFavorite: true
+    ),
+    
+    Product(
+        name: "Nike 2017 Blazer",
+        price: 2000,
+        image: "9",
+        isFavorite: true
+    ),
+
+]
