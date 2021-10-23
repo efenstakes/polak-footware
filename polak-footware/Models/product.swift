7a41b082dc537893ddf84ea8001405f99564a89c
Efen Stakes <efenstakes101@gmail.com>
2021-10-23 13:56:03 +0300

    update product data

--- polak-footware/Models/product.swift
+++ polak-footware/Models/product.swift
@@ -21,43 +21,43 @@ var products: [Product] = [
     Product(
         name: "Nike 2017 Blazer",
         price: 900,
-        image: "1",
-        isFavorite: true
+        image: "10",
+        isFavorite: false
     ),
     
     Product(
-        name: "Nike 2017 Blazer",
+        name: "Nike 2018 Blazer",
         price: 210,
         image: "2",
         isFavorite: true
     ),
     
     Product(
-        name: "Nike 2017 Blazer",
+        name: "Nike 2019 Blazer",
         price: 400,
         image: "3",
-        isFavorite: true
+        isFavorite: false
     ),
     
     Product(
-        name: "Nike 2017 Blazer",
+        name: "Nike 2020 Blazer",
         price: 450,
         image: "4",
-        isFavorite: true
+        isFavorite: false
     ),
     
     Product(
-        name: "Nike 2017 Blazer",
+        name: "Nike 2021 Blazer",
         price: 500,
         image: "5",
-        isFavorite: true
+        isFavorite: false
     ),
     
     Product(
         name: "Nike 2017 Blazer",
         price: 300,
         image: "6",
-        isFavorite: true
+        isFavorite: false
     ),
     
     Product(
@@ -71,7 +71,7 @@ var products: [Product] = [
         name: "Nike 2017 Blazer",
         price: 200,
         image: "8",
-        isFavorite: true
+        isFavorite: false
     ),
     
     Product(
